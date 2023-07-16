0f3444823c6cd19145a70c1386f059a7a6f9aae5
Brenno Kenji <brennokoj@hotmail.com>
2023-07-16 19:58:36 -0300

    Realizando testes de oop

--- test.py
+++ test.py
@@ -7,28 +7,48 @@ from tkcalendar import Calendar, DateEntry
 import locale
 import crud
 
-# Tipos de abas:
+# Realizando OOP:
 
-windows = tk.Tk()
-windows.title('Teste')
-windows.geometry('990x453')
+co0 = '#f0f3f5' # Preto
+co1 = '#feffff' # Branco
+co2 = '#4fa882' # Verde
+co3 = '#38576b' # Valor
+co4 = '#403d3d' # Letra
+co5 = '#e06636' # - Profit
+co6 = '#038cfc' # Azul
+co7 = '#ef5350' # Vermelho
+co8 = '#263238' # + Verde
+co9 = '#e9edf5' # Sky blue
 
-windows.resizable(width = False, height = False)
+class Ivy(tk.Tk):
+    def __init__(self):
 
-nb = ttk.Notebook(windows) # cria o campo para as abas
-nb.place(x = 0, y = 0, width = 990, height = 453)
+        #---------Criando_janela---------#
 
-tb1 = tk.Frame(nb)
-nb.add(tb1, text = 'exemplo1') # adiciona um frame como aba
+        super().__init__()
+        self.title('Ferramente de base de horas - Criada por Brenno Kenji (versão: 0.2)')
+        self.geometry('1000x463')
+        self.configure(background = co9)
+        self.resizable(width = False, height = False)
+        
+        #---------Dividindo_janela---------#
 
-tb2 = tk.Frame(nb)
-nb.add(tb2, text = 'exemplo2') 
+        guide = ttk.Notebook(self)
+        guide.place(x = 0, y = 0, height = 463, width = 1000)
 
-b_input_1 = tk.Button(tb1, text = 'botao1')
-b_input_1.place(x = 100, y = 50)
+        dashboard = tk.Frame(guide)
+        guide.add(dashboard, text = 'Dashboard')
 
-b_input_2 = tk.Button(tb2, text = 'botao2')
-b_input_2.place(x = 100, y = 50)
+        hours = tk.Frame(guide)
+        guide.add(hours, text = 'Controle de horas')
 
-windows.mainloop()
+        activies = tk.Frame(guide)
+        guide.add(activies, text = 'Controle de Atividades')
 
+        social = tk.Frame(guide)
+        guide.add(social, text = 'Forúm')
+
+
+if __name__ == '__main__':
+    ivy = Ivy()
+    ivy.mainloop()
